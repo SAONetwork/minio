c0a13bf26ab99d7def9db478c406ca3d37da55f8
vricale <dawnrhapsody@hotmail.com>
2023-08-10 18:24:47 +0800

    Revert "bug fix"
    This reverts commit 18df15e08aec9550e49bceb534b9c6b6a2665a9f.

--- cmd/erasure-object.go
+++ cmd/erasure-object.go
@@ -1409,33 +1409,29 @@ func (er erasureObjects) putObject(ctx context.Context, bucket string, object st
 	} else {
 		toEncode = data
 	}
-	partSize := data.Size()
-	if strings.HasPrefix(bucket, minioMetaBucket) {
-		if data.Size() > bigFileThreshold {
-			// We use 2 buffers, so we always have a full buffer of input.
-			bufA := er.bp.Get()
-			bufB := er.bp.Get()
-			defer er.bp.Put(bufA)
-			defer er.bp.Put(bufB)
-			ra, err := readahead.NewReaderBuffer(data, [][]byte{bufA[:fi.Erasure.BlockSize], bufB[:fi.Erasure.BlockSize]})
-			if err == nil {
-				toEncode = ra
-				defer ra.Close()
-			}
-			logger.LogIf(ctx, err)
+	if data.Size() > bigFileThreshold {
+		// We use 2 buffers, so we always have a full buffer of input.
+		bufA := er.bp.Get()
+		bufB := er.bp.Get()
+		defer er.bp.Put(bufA)
+		defer er.bp.Put(bufB)
+		ra, err := readahead.NewReaderBuffer(data, [][]byte{bufA[:fi.Erasure.BlockSize], bufB[:fi.Erasure.BlockSize]})
+		if err == nil {
+			toEncode = ra
+			defer ra.Close()
 		}
-		n, erasureErr := erasure.Encode(ctx, toEncode, writers, buffer, writeQuorum)
-		closeBitrotWriters(writers)
-		if erasureErr != nil {
-			return ObjectInfo{}, toObjectErr(erasureErr, minioMetaTmpBucket, tempErasureObj)
-		}
+		logger.LogIf(ctx, err)
+	}
+	n, erasureErr := erasure.Encode(ctx, toEncode, writers, buffer, writeQuorum)
+	closeBitrotWriters(writers)
+	if erasureErr != nil {
+		return ObjectInfo{}, toObjectErr(erasureErr, minioMetaTmpBucket, tempErasureObj)
+	}
 
-		// Should return IncompleteBody{} error when reader has fewer bytes
-		// than specified in request header.
-		if n < data.Size() {
-			return ObjectInfo{}, IncompleteBody{Bucket: bucket, Object: object}
-		}
-		partSize = n
+	// Should return IncompleteBody{} error when reader has fewer bytes
+	// than specified in request header.
+	if n < data.Size() {
+		return ObjectInfo{}, IncompleteBody{Bucket: bucket, Object: object}
 	}
 
 	var compIndex []byte
@@ -1468,7 +1464,7 @@ func (er erasureObjects) putObject(ctx context.Context, bucket string, object st
 			partsMetadata[i].Data = nil
 		}
 		// No need to add checksum to part. We already have it on the object.
-		partsMetadata[i].AddObjectPart(1, "", partSize, data.ActualSize(), modTime, compIndex, nil)
+		partsMetadata[i].AddObjectPart(1, "", n, data.ActualSize(), modTime, compIndex, nil)
 		partsMetadata[i].Erasure.AddChecksumInfo(ChecksumInfo{
 			PartNumber: 1,
 			Algorithm:  DefaultBitrotAlgorithm,
@@ -1498,7 +1494,7 @@ func (er erasureObjects) putObject(ctx context.Context, bucket string, object st
 	// Update `xl.meta` content on each disks.
 	for index := range partsMetadata {
 		partsMetadata[index].Metadata = userDefined
-		partsMetadata[index].Size = partSize
+		partsMetadata[index].Size = n
 		partsMetadata[index].ModTime = modTime
 	}
 
